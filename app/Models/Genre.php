deb1445aace45f133233eb2f67143828428ec57b
Serabass <>
2017-10-14 20:52:47 +0600

    PHPDoc for Genre model

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -4,6 +4,20 @@ namespace Bookshare\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Bookshare\Models\Genre
+ *
+ * @property int $id
+ * @property string $title
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Bookshare\Models\Book[] $books
+ * @method static \Illuminate\Database\Eloquent\Builder|\Bookshare\Models\Genre whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Bookshare\Models\Genre whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Bookshare\Models\Genre whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Bookshare\Models\Genre whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Genre extends Model
 {
     public function books()
