66b7dc2cfb4c11bd34e7f4744228a45d1a997f9a
Serabass <>
2017-10-14 16:46:17 +0600

    Tests

---
+++ tests/Feature/BookResourceTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Feature;
+
+use Bookshare\Models\Book;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class BookResourceTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testGetList()
+    {
+        $bookResponse = $this->get('/api/book');
+        $bookResponse->assertStatus(200);
+        $bookResponse->json();
+    }
+
+    public function testGetBook()
+    {
+        $randomBook = Book::inRandomOrder()->get()->first();
+        $bookResponse = $this->get('/api/book/' . $randomBook->id);
+        $bookResponse->assertStatus(200);
+        $bookResponse->assertJsonStructure([
+            'id',
+            'title',
+            'authorId',
+            'created_at',
+            'updated_at'
+        ]);
+        $bookResponse->assertJson($randomBook->toArray());
+    }
+
+    public function testStoreBook()
+    {
+        $book = [
+            'title' => 'War and peace'
+        ];
+        $storeResponse = $this->post('/api/book', $book);
+        $storeResponse->assertStatus(200);
+        $storeResponse->assertJsonStructure([
+            'status',
+            'data'
+        ]);
+        $json = $storeResponse->decodeResponseJson();
+        $id = $json['data']['id'];
+        $title = $json['data']['title'];
+        $this->assertTrue(is_numeric($id));
+        $this->assertEquals($title, $book['title']);
+
+        $bookResponse = $this->get('/api/book/' . $id);
+        $bookResponse->assertStatus(200);
+        $bookResponse->assertJsonStructure([
+            'id',
+            'title',
+            'authorId',
+            'created_at',
+            'updated_at'
+        ]);
+
+        $bookDeleteResponse = $this->delete('/api/book/' . $id);
+        $bookDeleteResponse->assertStatus(200);
+        $bookDeleteResponse->assertJsonStructure([
+            'status',
+            'data'
+        ]);
+        $json = $bookDeleteResponse->decodeResponseJson();
+        $this->assertEquals($json['status'], 'success');
+        $this->assertTrue(is_numeric($json['data']));
+
+        $this->assertNull(Book::find($id));
+    }
+}
