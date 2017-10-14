114d0118dfe17c2cde6a8915ce6e3d50b343d76e
Serabass <>
2017-10-14 20:35:16 +0600

    Added SandboxCommand

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace Bookshare\Console;
 
+use Bookshare\Console\Commands\SandboxCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
+        SandboxCommand::class
     ];
 
     /**
