b0f1fba87de26dd0ad29d7d56d0e73d86afcd34f
Serabass <>
2017-10-14 17:23:39 +0600

    Created OrderSeeder

---
+++ database/seeds/OrderSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Bookshare\Models\Book;
+use Bookshare\Models\Order;
+use Bookshare\User;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Seeder;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::dropIfExists('orders');
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('ownerId')->nullable();
+            $table->integer('bookId')->nullable();
+            $table->integer('receiverId')->nullable();
+            $table->dateTime('returnDate')->nullable();
+            $table->string('comment');
+            $table->timestamps();
+        });
+
+        factory(Order::class, DatabaseSeeder::COUNT)->create()->each(function (Order $u) {
+            $u->owner()->associate(User::inRandomOrder()->first())->save();
+            $u->receiver()->associate(User::inRandomOrder()->first())->save();
+            $u->book()->associate(Book::inRandomOrder()->first())->save();
+        });
+    }
+}
