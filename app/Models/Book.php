ac4898c119d8091be68ea450df91eda07df19a6e
Serabass <>
2017-10-14 16:46:44 +0600

    Models and relations

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,25 @@
 
 namespace Bookshare\Models;
 
+use Bookshare\User;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Book
+ * @property string $title
+ * @property Author $author
+ * @property User $user
+ * @package Bookshare\Models
+ */
 class Book extends Model
 {
-    //
+    public function author()
+    {
+        return $this->belongsTo(Author::class, 'authorId');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
 }
