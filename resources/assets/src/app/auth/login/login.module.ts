cb1287325589d42675bf40479514436462526363
Serabass <>
2017-10-15 13:39:42 +0600

    Frontend WIP

--- resources/assets/src/app/auth/login/login.module.ts
+++ resources/assets/src/app/auth/login/login.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { LoginRoute } from './login-routing.module';
-import { LoginComponent } from './login.component';
+import {LoginRoute} from './login-routing.module';
+import {LoginComponent} from './login.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {CookieModule} from 'ngx-cookie';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    LoginRoute
-  ],
-  declarations: [
-  	LoginComponent
-  ]
+    imports: [
+        CommonModule,
+        LoginRoute,
+        FormsModule,
+        HttpClientModule,
+        CookieModule.forRoot()
+    ],
+    declarations: [
+        LoginComponent
+    ]
 })
-export class LoginModule { }
+export class LoginModule {}
