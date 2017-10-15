caf2cd4f25dee0f428c5ac45466f62119de95338
Serabass <>
2017-10-15 09:57:56 +0600

    Added method defaultAuth to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,17 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function defaultAuth()
+    {
+        $authRequest = $this->post('/api/login', [
+            'email' => 'fake@mail.ru',
+            'password' => 'secret'
+        ]);
+        $authRequest->assertStatus(200);
+
+        $json = $authRequest->json();
+        $this->assertNotEmpty($json['auth_token']);
+        return $json;
+    }
 }
