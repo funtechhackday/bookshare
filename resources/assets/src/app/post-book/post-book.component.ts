eae4623752725eb0c99d2a8ccfad0ef5349e839d
Serabass <>
2017-10-15 17:44:26 +0600

    postbook fix

--- resources/assets/src/app/post-book/post-book.component.ts
+++ resources/assets/src/app/post-book/post-book.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Router} from "@angular/router";
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-post-book',
