10f989de23d974eec1d77f38ce5e72b7902f114b
Serabass <>
2017-10-15 15:14:05 +0600

    added ngx-cookie

---
+++ app/Http/Controllers/Api/GenreController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bookshare\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Bookshare\Http\Controllers\Controller;
+
+class GenreController extends Controller
+{
+    //
+}
